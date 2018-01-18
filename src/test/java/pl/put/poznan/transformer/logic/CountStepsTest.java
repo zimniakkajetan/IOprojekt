5f91f44cbc93ff934d8fd7628935980a5587726b
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2018-01-18 18:52:17 +0100

    -modify move tests between classes

---
+++ src/test/java/pl/put/poznan/transformer/logic/CountStepsTest.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountStepsTest {
+    @Test
+    public void emptyScenarioCountStepsRequestClass(){
+        String[] emptyScenario = {};
+        String response, expected;
+
+        response = IRequest.getRequest("policzkroki",emptyScenario).getName();
+        expected = new CountStepsRequest().getName();
+        Assert.assertEquals(expected,response);
+
+        String strResponse = new CountStepsRequest().run(emptyScenario);
+        String strExpected = "\"" + "Ilosc krokow jest rowna " + Integer.toString(0) + "\"";
+        Assert.assertEquals(strExpected, strResponse);
+    }
+
+    @Test
+    public void validCountCountStepsRequestClass(){
+        CountStepsRequest countStepsRequest = new CountStepsRequest();
+        String expected;
+        List<String> scenarioList = new ArrayList<String>();
+
+        for(int i = 1; i < 25; i++) {
+            scenarioList.add("1");
+            String[] scenario = new String[scenarioList.size()];
+            scenario = scenarioList.toArray(scenario);
+
+            expected = "\"" + "Ilosc krokow jest rowna " + Integer.toString(i) + "\"";
+            Assert.assertEquals(expected, countStepsRequest.run(scenario));
+
+        }
+    }
+
+}
